3a65d386aa3b58e46a7636c109add6890721c80c
Yosuke Sugishita <yosuke.sugi@gmail.com>
2014-02-25 15:18:24 -0800

    Wrote code to update pi

--- gmm_clustering.py
+++ gmm_clustering.py
@@ -4,11 +4,6 @@ This implements GMM (Gausian Mixture Model) clustering.
 
 To run this, use something like:
     ../../bin/pyspark gmm_clustering.py local kmeans_data.txt 2 0.1
-
-
-<CREDIT>
-I have used some methods from: 
-    https://github.com/apache/incubator-spark/blob/master/python/examples/kmeans.py
 """
 
 
@@ -29,8 +24,11 @@ def parseVector(line):
 # input: point
 # output: (point, numpy_array_of_responsibilities)
 # TODO: implement this
-def computeReponsibility(point):
-    """Implement this"""
+def responsibilities(point, pi, centers, cov_matrices):
+    r = np.zeros(len(pi))
+    for k in range(0, len(pi)):
+        r[k] = pi[k] * multivariate_normal(centers[k], cov_matrices[k]).pdf(point)
+    return r / sum(r)
 
 if __name__ == "__main__":
     # TODO: edit the following part later
@@ -43,8 +41,9 @@ if __name__ == "__main__":
     lines = sc.textFile(sys.argv[2])
     data_points = lines.map(parseVector).cache()
     num_clusters = int(sys.argv[3])
+
     num_points = data_points.count()
-    print("number of points we have here:", num_points)
+    print("Number of points we have here:", num_points)
 
     # Initialize parameters
     centers = data_points.take(num_clusters) # randomly choose points to initialize the centers
@@ -52,16 +51,16 @@ if __name__ == "__main__":
     cov_matrices = [ np.identity(dimension) ] * num_clusters
     pi = np.ones(num_clusters) / num_clusters
 
-
-    # TODO: Haven't tested the following parts yet.
     # E Step: Compute the responsibilities
-    resp = data_points.map(computeReponsibility) #TODO DOING NOW
+    resp = data_points.map(lambda point: (point, responsibilities(point, pi, centers, cov_matrices) ) )
     # format of resp: (point, numpy_array_of_responsibilities)
         
     # M Step: Update the parameters
     # In the following code, (p1, r1) stands for (point_1, responsibility_1)
-    pi = resp.reduce(lambda (p1, r1), (p2, r2): r1 + r2) # we don't need points here, so ignore them.
-    mean = resp.reduce(lambda (p1, r1), (p2, r2): p1 * r1 + p2 * r2) / (N * pi)
+    pi = resp.reduce(lambda a, b: a) # we don't need points here, so ignore them.
+#    pi = resp.reduce(lambda (p1, r1), (p2, r2): r1 + r2) # we don't need points here, so ignore them.
+#    print ("pi, pi)
+#    mean = resp.reduce(lambda (p1, r1), (p2, r2): p1 * r1 + p2 * r2) / (N * pi)
 #    cov_matrices = resp.reduce(lambda (p1, r1), (p2, r2): ):
     
     #centers = 
