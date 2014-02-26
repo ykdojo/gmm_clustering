deb66c887591cdf141e432027706773c828048b7
Yosuke Sugishita <yosuke.sugi@gmail.com>
2014-02-25 23:26:17 -0800

    More on M Step.

--- gmm_clustering.py
+++ gmm_clustering.py
@@ -23,12 +23,17 @@ def parseVector(line):
 
 # input: point
 # output: (point, numpy_array_of_responsibilities)
-# TODO: implement this
 def responsibilities(point, pi, centers, cov_matrices):
     r = np.zeros(len(pi))
     for k in range(0, len(pi)):
         r[k] = pi[k] * multivariate_normal(centers[k], cov_matrices[k]).pdf(point)
     return r / sum(r)
+
+# input: numpy_array
+# output: numpy_array * numpy_array_transposed (in matrix)
+def transpose_and_multiply(np_array):
+    # need to cast to matrix in order to tranpose a one-dimentional array
+    return np.transpose(np.matrix(np_array)).dot(np.matrix(np_array))
 
 if __name__ == "__main__":
     # TODO: edit the following part later
@@ -56,11 +61,15 @@ if __name__ == "__main__":
     # format of resp: (point, numpy_array_of_responsibilities)
         
     # M Step: Update the parameters
-    # In the following code, (p1, r1) stands for (point_1, responsibility_1)
-    pi = resp.reduce(lambda a, b: a) # we don't need points here, so ignore them.
-#    pi = resp.reduce(lambda (p1, r1), (p2, r2): r1 + r2) # we don't need points here, so ignore them.
-#    print ("pi, pi)
-#    mean = resp.reduce(lambda (p1, r1), (p2, r2): p1 * r1 + p2 * r2) / (N * pi)
+    # In the following part, (p1, r1) stands for (point_1, responsibility_1)
+    pi = resp.reduce(lambda (p1, r1), (p2, r2) : r1 + r2) / num_points  # we don't need points here, so ignore them.
+    temp = resp.map(lambda (p, r): np.array([p * i for i in r])) # make a 2-d array
+    centers = temp.reduce(lambda tmp1, tmp2: tmp1 + tmp2) / num_points / np.array([[i] * dimension for i in pi])
+
+    transpose_and_multiply
+    temp2 = np.array([transpose_and_multiply(c) for c in centers ])
+    
+    import pdb; pdb.set_trace()
 #    cov_matrices = resp.reduce(lambda (p1, r1), (p2, r2): ):
     
     #centers = 
